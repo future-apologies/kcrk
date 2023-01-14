65b4cca5db7e67b025fa57141d4c58b1e59daffe
Heather White <miss.pomykal@gmail.com>
2023-01-13 19:04:32 -0600

    initial edits. not done.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,8 +3,16 @@ import React from 'react';
 const Navbar = () => {
   return (
       <>
-        <nav>
-          navigation station.
+        <nav className="navbar">
+            <h1>navigation station.</h1>
+            <div className="links">
+                <Link to="/">Home</Link>
+                <Link to="/create" style={{ 
+                    color: 'white', 
+                    backgroundColor: '#f1356d',
+                    borderRadius: '8px' 
+                }}>New Blog</Link>
+             </div>
         </nav>
       </>
   );
