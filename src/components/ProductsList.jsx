97b45dc1411265909ee9298680c7975fd6056e06
jkentwhite <jkentwhite@gmail.com>
2023-01-30 16:01:50 -0600

    all products basic css and link to single product

--- src/components/ProductsList.jsx
+++ src/components/ProductsList.jsx
@@ -1,13 +1,28 @@
+import { Link } from "react-router-dom";
 
+// pass in the products list from the fetch call as productsList
 const ProductsList = ({productsList, title}) => {
   
   return (
     <div className="product-list">
+      {/* map over every product in productsList */}
        {productsList.map((product) => (
-           <div className="product-preview" key={product.sku}>
-               <h2>{ product.name }</h2>
-               <p>{ product.description }</p>
-               {product.imgUrl && <img src={ product.imgUrl } alt="product"/>}
+
+            // set the key equal to the sku of each product
+           <div className="product-preview" key={product.id}>
+
+             {/* set the link to each single product view by passing the id/sku as an identifier */}
+             <Link to={ `/productsList/${product.id}` }>
+
+               {/* if the product has an image, display it using the url in the json data */}
+                {product.imgUrl && <img src={ product.imgUrl } alt="product"/>}
+                <div className="product-info">
+                  
+                  {/* display each product's name and description */}
+                  <h2>{ product.name }</h2>
+                  <p>{ product.description }</p>
+                </div>
+            </Link>
            </div>
        ))}
 
